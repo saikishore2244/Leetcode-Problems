9b7186b0c5552538af5f94e4708bbd09cb6c44ea
Sai Kishore <saikishore9665@gmail.com>
2022-07-14 20:18:39 +0530

    141. Linked List Cycle

--- Easy/LinkedLists/141. Linked List Cycle.js
+++ Easy/LinkedLists/141. Linked List Cycle.js
@@ -1,3 +1,8 @@
+//********************** BEST SOLUTION **********************************/
+/*
+Time Complexity - O(n)
+Space Complexity - O(1)
+*/
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -19,4 +24,23 @@
     head.isVisited = true;
     
     return hasCycle(head.next);
+};
+/*
+    Floyd's cycle finding algorithm - 2 pointers
+    one moves double the speed of the other, eventually :-
+    if(they coincide) means cycle exists
+    else if exception is thrown end of the linked list came , no cycle was detected
+*/
+const hasCycleFloyds = function(head) {
+    try {
+        let slowPointer =head, fastPointer = head;
+        while (true) {
+            slowPointer = slowPointer.next
+            fastPointer = fastPointer.next.next
+            if (slowPointer === fastPointer)
+                return true
+        }
+    } catch (e) {
+        return false
+    }
 };

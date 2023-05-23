03d1b9114c39d12512fa1b6ed5f9d1d9c729ba9a
Sai Kishore <saikishore9665@gmail.com>
2023-05-23 15:54:45 +0530

    Testing Prettier

--- Revise/20. Valid Parentheses.js
+++ Revise/20. Valid Parentheses.js
@@ -4,20 +4,20 @@
  * @return {boolean}
  */
 var isValid = (s) => {
-    if(s.length % 2 != 0) return false;
-    var stack = [];
-    let isValid = false;
-    for(let c of s){
-        if(c == '('){
-           stack.push(')');
-        }else if(c == '['){
-           stack.push(']');   
-        }else if(c == '{'){
-           stack.push('}');   
-        }else if(c != stack.pop()){
-            return false;
-        }
+  if (s.length % 2 != 0) return false;
+  var stack = [];
+  let isValid = false;
+  for (let c of s) {
+    if (c == "(") {
+      stack.push(")");
+    } else if (c == "[") {
+      stack.push("]");
+    } else if (c == "{") {
+      stack.push("}");
+    } else if (c != stack.pop()) {
+      return false;
     }
-    stack.length == 0 ?  isValid = true : ''; 
-    return isValid;
+  }
+  stack.length == 0 ? (isValid = true) : "";
+  return isValid;
 };

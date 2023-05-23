6e55fcf4d953b51a9906dd9a8baad68b1d5b03a6
Sai Kishore <skanteti@paloaltonetworks.com>
2023-05-23 15:46:15 +0530

    Testing Prettier

--- Revise/21. Merge Two Sorted Lists.js
+++ Revise/21. Merge Two Sorted Lists.js
@@ -15,24 +15,24 @@ Space Complexity - O(M+N)
  * @param {ListNode} list2
  * @return {ListNode}
  */
- var mergeTwoLists = function(list1, list2) {
-    let current = new ListNode();
-    let dummy = current;
-    while(list1 !== null && list2 !== null){
-        if(list1.val < list2.val){
-            current.next = list1;
-            list1 = list1.next;
-        }else{
-            current.next = list2;
-            list2 = list2.next;            
-        }
-        current = current.next;
+var mergeTwoLists = function (list1, list2) {
+  let current = new ListNode();
+  let dummy = current;
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
     }
-    if(list1 !== null){
-       current.next = list1;
-    }
-    if(list2 !== null){
-       current.next = list2;
-    }
-    return dummy.next;
+    current = current.next;
+  }
+  if (list1 !== null) {
+    current.next = list1;
+  }
+  if (list2 !== null) {
+    current.next = list2;
+  }
+  return dummy.next;
 };

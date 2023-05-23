70c559b409976789f31c7eefa5b27b23fdcbbaa3
Sai Kishore <skanteti@paloaltonetworks.com>
2023-05-23 15:35:25 +0530

    setting up prettier & testing it

--- Revise/1. Two Sum.js
+++ Revise/1. Two Sum.js
@@ -8,18 +8,18 @@ Space Complexity - O(n) Map is created and stores all values
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-    var numsMap= new Map();
-    for(let i=0;i<nums.length;i++){
-     let complement = target-nums[i];
-        if(numsMap.has(complement)){
-           return [numsMap.get(complement),i];
-         }
-        numsMap.set(nums[i],i);
+var twoSum = function (nums, target) {
+  var numsMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (numsMap.has(complement)) {
+      return [numsMap.get(complement), i];
     }
+    numsMap.set(nums[i], i);
+  }
 };
 
-let nums = [3,4,5,6,3];
+let nums = [3, 4, 5, 6, 3];
 let target = 7;
-console.log(nums,target);
-console.log(twoSum(nums,target));
+console.log(nums, target);
+console.log(twoSum(nums, target));
